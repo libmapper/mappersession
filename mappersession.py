7b6c7773592e8e600d58c79a4c91fcabbe98d46f
bboettcher31 <bradyboettcher@gmail.com>
2022-10-02 00:24:21 -0400

    midway through saving maps

--- mappersession.py
+++ mappersession.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import json
 import jsonschema
 from jsonschema import validate
 import libmapper as mpr
@@ -16,7 +17,7 @@ def main():
         print("Not enough arguments provided, please use --save or --load with JSON file path(s)")
         return
 
-def save(file, description, viewName="", views={}):
+def save(filename, description, viewName="", views={}):
     """saves the current mapping state as a JSON session file.
     
     :param file: The JSON file to save the session into 
@@ -28,19 +29,53 @@ def save(file, description, viewName="", views={}):
 
     # Create JSON from network state following the schema
     session = {}
-    session.fileversion = "2.3"
-    session.description = description
-    session.views = views
+    session["fileversion"] = "2.3"
+    session["description"] = description.strip("'")
+    session["views"] = views
 
     # Populate maps
-    session.maps = []
+    g = mpr.Graph()
+    print("Collecting session state from network...")
+    g.poll(1000)
+    session["maps"] = []
+    for mapIdx in range(len(g.maps())):
+        newMap = {}
+        # Source signals
+        srcSigs = g.maps()[mapIdx].signals(mpr.Location.SOURCE)
+        newMap["sources"] = []
+        for srcIdx in range(len(srcSigs)):
+            srcName = (srcSigs[srcIdx].device().get_property(mpr.Property.NAME) +
+                        "/" + srcSigs[srcIdx].get_property(mpr.Property.NAME))
+            newMap["sources"].append(srcName)
+        # Destination signals
+        dstSigs = g.maps()[mapIdx].signals(mpr.Location.DESTINATION)
+        newMap["destinations"] = []
+        for dstIdx in range(len(dstSigs)):
+            dstName = (dstSigs[dstIdx].device().get_property(mpr.Property.NAME) +
+                        "/" + dstSigs[dstIdx].get_property(mpr.Property.NAME))
+            newMap["destinations"].append(dstName)
+
+        # Other properties
+        newMap["expression"] = g.maps()[mapIdx].get_property(mpr.Property.EXPRESSION)
+        newMap["muted"] = g.maps()[mapIdx].get_property(mpr.Property.MUTED)
+        newMap["process_loc"] = g.maps()[mapIdx].get_property(mpr.Property.PROCESS_LOCATION)
+        #newMap["protocol"] = g.maps()[mapIdx].get_property(mpr.Property.PROTOCOL)
+        #newMap["scope"] = g.maps()[mapIdx].get_property(mpr.Property.SCOPE)
+        newMap["use_inst"] = g.maps()[mapIdx].get_property(mpr.Property.USE_INSTANCES)
+        newMap["version"] = g.maps()[mapIdx].get_property(mpr.Property.VERSION)
+        # Add to maps
+        session["maps"].append(newMap)
+
+    print(g.maps()[0].get_property(mpr.Property.EXPRESSION))
+    
 
     # Add unmapped destination signal values
-    session.destinationValues = []
+    session["destinationValues"] = []
 
     # Save into the file
-    with open(file, 'w', encoding='utf-8') as f:
+    with open(filename.strip("'"), 'w', encoding='utf-8') as f:
         json.dump(session, f, ensure_ascii=False, indent=4)
+    print("Saved session as: " + filename)
 
 def load(files):
     """loads one or more sessions with options for staging and cycling.
@@ -57,7 +92,7 @@ def createParser():
         metavar='PATH',
         help="Session JSON file(s) to load")
     parser.add_argument(
-        '--save', type=argparse.FileType('w'),
+        '--save', type=ascii,
         metavar='PATH',
         help="Save session as JSON file")
     parser.add_argument(

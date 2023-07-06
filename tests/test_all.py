50d2036539534be4a7fafa554e252476b85dc3e4
Joseph Malloch <jmalloch@dal.ca>
2023-07-06 14:49:25 -0300

    test_all.py: Enable loading mappersession module from local source (rather than requiring installed copy).

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,7 +1,20 @@
-import mappersession as session
 import libmapper as mpr
 import json
 import os
+import sys
+
+try:
+    import mappersession as session
+except:
+    try:
+        sys.path.append(
+                        os.path.join(os.path.join(os.getcwd(),
+                                                  os.path.dirname(sys.argv[0])),
+                                     '../src/mappersession'))
+        import mappersession as session
+    except:
+        print('Error importing mappersession module.')
+        sys.exit(1)
 
 # An end-to-end test case for saving, loading and clearing sessions
 if __name__ == '__main__':
